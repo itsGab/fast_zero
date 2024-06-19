71d0dd3ac1dd96a2a5274d490b6a28e9cfd5a234
itsGab <gabriel.schultz10@gmail.com>
2024-06-18 21:45:30 -0300

    aula 2 schemas e mais

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', response_model=Message, status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html/', response_class=HTMLResponse, status_code=HTTPStatus.OK)
+def read_html():
+    return """
+    <html>
+      <head>
+        <title>Nosso ola mundo!</title>
+      </head>
+      <body>
+        <h1> Ola Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,10 +5,27 @@ from fastapi.testclient import TestClient
 from fast_zero.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
+def test_read_root_deve_retornar_ok_e_ola_mundo():
     client = TestClient(app)  # arrange
 
-    response = client.get('/') # act
+    response = client.get('/')  # act
 
-    assert response.status_code == HTTPStatus.OK # assert
+    assert response.status_code == HTTPStatus.OK  # assert
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_read_html_deve_retorno_ok_e_html_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/html/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == """
+    <html>
+      <head>
+        <title>Nosso ola mundo!</title>
+      </head>
+      <body>
+        <h1> Ola Mundo </h1>
+      </body>
+    </html>"""
